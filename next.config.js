6dbaea2455c2f4c92ace828649113fb28a9311ef
Naveed-Shoukat <shoukat.naveed@gmail.com>
2022-04-15 18:59:22 +0300

    eslintrc problem

--- next.config.js
+++ next.config.js
@@ -1,32 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-  images: { domains: ['res.cloudinary.com'] },
-}
-
-
-
-/*
-
-@type {import('next').NextConfig} 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  reactStrictMode: true,
   images: { domains: ['res.cloudinary.com'] },
-  nextConfig,
-  compiler: {
-    styledComponents: true,
-  },
-}
-
-old file
-const nextConfig = {
-  reactStrictMode: true,
 }
 
 module.exports = nextConfig
-
-
-*/
